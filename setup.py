b5e1729fb407ee3cf908ee24da16a48d3643a038
Foad Abo Dahood <Foad.Abo.Dahood@ibm.com>
2023-01-19 14:16:44 +0200

    Init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pyrallel_process',
+    version='1.0',
+    description='A library to parallelize the execution of a function in python',
+    author='Foad Abo Dahood',
+    author_email='your_email@example.com',
+    py_modules=['pyrallel_process'],
+    install_requires=['futures'],
+)
